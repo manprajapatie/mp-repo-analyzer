7b2b89022c6b934b93f6cc51e01d3ed52581c1af
manprajapatie <manprajapatie@gmail.com>
2026-03-23 08:18:28 +0530

    feat : add react Router and Setup page based on it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header/>
+      <AppRoutes/>
+      <Footer/>
     </>
   )
 }
